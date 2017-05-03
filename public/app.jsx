78c0dcafc73ca7eed7b8ce48a731c9bbeb327071
Praveenkumar V <venugopalapanickerpraveenkumar@johndeere.com>
2017-05-03 12:36:41 +0530

    form submit

--- public/app.jsx
+++ public/app.jsx
@@ -5,6 +5,11 @@ var Greeter = React.createClass({
           message:'Welcome to the new world of Blah blah blah...'
       }
     },
+    onBtnClick:function (e) {
+      e.preventDefault();
+      var user = this.refs.user.value;
+      alert(user);
+    },
     render: function () {
         var name = this.props.name;
         var message = this.props.message;
@@ -12,6 +17,11 @@ var Greeter = React.createClass({
             <div>
                 <h1> Hello {name}!</h1>
                 <p>{message}</p>
+
+                <form onSubmit={this.onBtnClick}>
+                    <input type="text" ref="user"/>
+                    <button>Set Name</button>
+                </form>
             </div>
         );
     }
