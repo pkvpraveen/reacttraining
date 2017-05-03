38a13a8e3cd19a021cc68d74cd16a73e10f491e1
Praveenkumar V <venugopalapanickerpraveenkumar@johndeere.com>
2017-05-03 12:24:07 +0530

    aded another default prop message

--- public/app.jsx
+++ public/app.jsx
@@ -1,14 +1,17 @@
 var Greeter = React.createClass({
     getDefaultProps: function () {
       return{
-          name:'React'
+          name:'React',
+          message:'Welcome to the new world of Blah blah blah...'
       }
     },
     render: function () {
         var name = this.props.name;
+        var message = this.props.message;
         return(
             <div>
                 <h1> Hello {name}!</h1>
+                <p>{message}</p>
             </div>
         );
     }
