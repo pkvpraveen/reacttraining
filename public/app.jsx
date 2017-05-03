c73fa33eea986fa0703993b9b1a12bd873446c23
Praveenkumar V <venugopalapanickerpraveenkumar@johndeere.com>
2017-05-03 12:01:55 +0530

    use of prop

--- public/app.jsx
+++ public/app.jsx
@@ -1,14 +1,14 @@
 var Greeter = React.createClass({
     render: function () {
+        var name = this.props.name;
         return(
             <div>
-                <h1> Hello React!</h1>
-                <p>This form a component!</p>
+                <h1> Hello {name}!</h1>
             </div>
         );
     }
 });
 ReactDOM.render(
-    <Greeter/>,
+    <Greeter name="Praveen"/>,
     document.getElementById('app')
 );
