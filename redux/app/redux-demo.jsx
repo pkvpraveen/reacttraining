260149d76e9e66885ae4f5e3642dc53c9af6e279
Praveenkumar V <VenugopalaPanickerPraveenKumar@JohnDeere.com>
2017-05-05 12:09:30 +0530

    added array  to state

--- redux/app/redux-demo.jsx
+++ redux/app/redux-demo.jsx
@@ -1,31 +1,48 @@
 import {createStore} from 'redux';
 
-const reducer = (state = {name: 'Anonymous'}, action) => {
+let initialState = {name: 'Anonymous', hobbies: []};
+let hobbyId = 1;
+const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'CHANGE_NAME':
             return {
                 ...state,
                 name: action.data
             };
-
+        case 'ADD_HOBBY':
+            return {
+                ...state,
+                hobbies: [
+                    ...state.hobbies,
+                    {
+                        id: hobbyId++,
+                        hobby: action.data
+                    }
+                ]
+            };
         default:
             return state;
     }
 };
 
 const store = createStore(reducer);
-store.subscribe(()=>{
+store.subscribe(() => {
     let state = store.getState();
-    console.log( state);
+    console.log(state);
 });
 
 
 store.dispatch({
-    type:'CHANGE_NAME',
-    data:'Praveen'
+    type: 'CHANGE_NAME',
+    data: 'Praveen'
 });
 
 store.dispatch({
-    type:'CHANGE_NAME',
-    data:'Praveen Kumar'
+    type: 'CHANGE_NAME',
+    data: 'Praveen Kumar'
+});
+
+store.dispatch({
+    type: 'ADD_HOBBY',
+    data: 'Swimming'
 });
